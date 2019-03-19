6e44f5f9637b4bdbfab13ce152eaa3075152cd2f
dealant <alan361206@gmail.com>
2019-03-18 22:46:13 -0700

    project complete

--- src/root/nested/contactinfoextractor.py
+++ src/root/nested/contactinfoextractor.py
@@ -11,5 +11,39 @@ paste results to clipboard
 import pyperclip, re
 
 phonenum = re.compile(r'''(
-    (/d{3}|/(/d{3}/))? #area code
-    )
+    (\d{3}|\(\d{3}\))?                # area code
+    (\s|-|\.)?                        # separator
+    (\d{3})                           # first 3 digits
+    (\s|-|\.)                         # separator
+    (\d{4})                           # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
+    )''', re.VERBOSE)
+
+#create email regex
+emailreg = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+             #user name
+    @                           #the @ sign 
+    [a-zA-Z0-9.-]+             #domainname
+    (\.(com|org|net|gov|edu))    #domain type
+    )''', re.VERBOSE)
+
+# TODO: Find matches in clipboard text.
+
+text = str(pyperclip.paste())
+matches = []
+for groups in phonenum.findall(text):
+    phonefind = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phonefind += ' x' + groups[8]
+    matches.append(phonefind)
+for groups in emailreg.findall(text):
+    matches.append(groups[0])
+
+# TODO: Copy results to the clipboard.
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
