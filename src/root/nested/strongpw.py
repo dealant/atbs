512e7e22220f0cf7755f95f09ac50b242d47a64b
dealant <alan361206@gmail.com>
2019-03-21 23:08:11 -0700

    nochang

---
+++ src/root/nested/strongpw.py
@@ -0,0 +1,10 @@
+#! python3
+import pyperclip, re
+
+# TODO: make a Regex at least 8 characters.
+
+8chr = re.compile(/d{8,})
+
+# TODO: make a Regex contains both uppercase and lowercase letters
+
+# TODO: make a regex that has at least one digit
