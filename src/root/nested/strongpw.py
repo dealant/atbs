2dfb62cf5361953cc362c8e555ce1089d39b0280
dealant <alan361206@gmail.com>
2019-03-21 23:26:10 -0700

    the uppercase and lower case and digit regex is in order only

--- src/root/nested/strongpw.py
+++ src/root/nested/strongpw.py
@@ -3,8 +3,10 @@ import pyperclip, re
 
 # TODO: make a Regex at least 8 characters.
 
-8chr = re.compile(/d{8,})
+pwlength = re.compile(r'[a-zA-Z0-9._%+-]{8,}')
 
 # TODO: make a Regex contains both uppercase and lowercase letters
-
+pwcaseandnum = re.compile(r'[a-z]+[A-Z]+[0-9]+')
 # TODO: make a regex that has at least one digit
+
+# TODO: check password from user against all three regex
