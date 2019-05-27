e09bff2b8cffbf1f46b16138b1b781e4eaf673ba
alansuface <alansuface@192.168.86.23>
2019-05-26 23:04:52 -0700

    lucky search thing

---
+++ src/root/nested/lucky.py
@@ -0,0 +1,22 @@
+'''
+Created on May 26, 2019
+
+@author: alansuface
+This program:
+Gets search keywords from the command line arguments.
+Retrieves the search results page.
+Opens a browser tab for each result
+
+Fucntionally this means your code will need to do the following:
+Read the command line arguments from sys.argv.
+Fetch the search result page with the requests module.
+Find the links to each search result.
+Call the webbrowser.open() function to open the web browser.
+'''
+
+import sys, requests, pyperclip
+
+#TODO: Gets search keywords from the command line arguments.
+
+#TODO: Retrieves the search results page.
+#TODO: Opens a browser tab for each result
--- src/root/nested/webscraper.py
+++
@@ -1,5 +0,0 @@
-'''
-Created on May 26, 2019
-
-@author: alansuface
-'''
